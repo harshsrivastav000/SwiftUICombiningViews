94a7b0732856817f9ac45f6d155ad49e498ec1ff
Harsh Srivastav <harsh.s@demansol.com>
2020-01-10 12:13:36 +0530

    Initial Commit

---
+++ SwiftUIImplementation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIImplementation
+//
+//  Created by harsh on 10/01/20.
+//  Copyright © 2020 harsh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
